f651c0867647a7d07a48488459f11c972a0bd901
mikezks <23002661+mikezks@users.noreply.github.com>
2023-05-22 19:54:37 +0200

    init: adv workspace

---
+++ apps/tickets/src/main.ts
@@ -0,0 +1,20 @@
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+import { bootstrapSignalApplication } from '@flight-demo/shared/util-signals';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+    importProvidersFrom(MatDialogModule),
+  ],
+});
